9a5417c18fa261f50adbdfd9382c3fe492cde744
Sadman-123 <19201103123@cse.bubt.edu.bd>
2025-01-29 01:41:16 +0600

    initially routing done

--- lib/application_layer/app.dart
+++ lib/application_layer/app.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sixbites/presentation_layer/ui/home.dart';
 class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => Home(),)
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sixbites/application_layer/app.dart';
 void main()
 {
   runApp(App());
