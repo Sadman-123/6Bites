cd20fb73df29e4326f32e8165f0a66db5ebd55d5
Sadman-123 <19201103123@cse.bubt.edu.bd>
2025-01-29 02:27:29 +0600

    Dependency Injection

--- lib/presentation_layer/ui/pages/app_pages/home.dart
+++ lib/presentation_layer/ui/pages/app_pages/home.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sixbites/presentation_layer/state_holders/home_controller.dart';
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    HomeController home=Get.find();
     return Scaffold(
       body: Center(child: Text("App"),),
     );
