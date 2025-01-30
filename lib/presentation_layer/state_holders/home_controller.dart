cae7ae8a9bdb625eefa8e1ec8f03512c6b4ddc08
Sadman-123 <19201103123@cse.bubt.edu.bd>
2025-01-30 11:23:42 +0600

    track the loaded data also got new progressbar

--- lib/presentation_layer/state_holders/home_controller.dart
+++ lib/presentation_layer/state_holders/home_controller.dart
@@ -2,73 +2,80 @@ import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:sixbites/data_layer/api_urls.dart';
-class HomeController extends GetxController{
+
+class HomeController extends GetxController {
+  RxList<dynamic> category_list = <dynamic>[].obs;
+  RxList<dynamic> popular_list = <dynamic>[].obs;
+  RxList<dynamic> campaign_list = <dynamic>[].obs;
+  RxList<dynamic> restaurant_list = <dynamic>[].obs;
+  RxBool isLoaded = false.obs;
   @override
   void onInit() {
     super.onInit();
-    getCategories();
-    getPopular();
-    getCampaign();
-    getRestaurants();
+    fetchInitialData();
   }
-  RxList<dynamic> category_list=[].obs;
-  RxList<dynamic> popular_list=[].obs;
-  RxList<dynamic> campaign_list=[].obs;
-  RxList<dynamic> restaurant_list=[].obs;
-  Future<void>getCategories()async
-  {
-    var url=Uri.parse(ApiUrls.Category_List);
-    var res=await http.get(url,headers: {
-      'Content-Type':'application/json',
-      'zoneId':'[1]',
-      'latitude':'23.735129',
-      'longitude':'90.425614'
-    });
-    if(res.statusCode==200)
-      {
-        category_list.value=jsonDecode(res.body);
+  void fetchInitialData() async {
+    isLoaded.value = false;
+    await Future.wait([
+      getCategories(),
+      getPopular(),
+      getCampaign(),
+      getRestaurants(),
+    ]);
+    isLoaded.value = true;
+  }
+  Future<void> getCategories() async {
+    await _fetchData(
+      url: ApiUrls.Category_List,
+      list: category_list,
+    );
+  }
+  Future<void> getPopular() async {
+    await _fetchData(
+      url: ApiUrls.Popular_List,
+      list: popular_list,
+      dataKey: 'products',
+    );
+  }
+  Future<void> getCampaign() async {
+    await _fetchData(
+      url: ApiUrls.Campaign_List,
+      list: campaign_list,
+    );
+  }
+  Future<void> getRestaurants() async {
+    await _fetchData(
+      url: ApiUrls.Restaurant_List,
+      list: restaurant_list,
+      dataKey: 'restaurants',
+    );
+  }
+  Future<void> _fetchData({
+    required String url,
+    required RxList<dynamic> list,
+    String? dataKey,
+  }) async {
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+        headers: _MyHeaders(),
+      );
+      if (response.statusCode == 200) {
+        final decodedData = jsonDecode(response.body);
+        list.value = dataKey != null ? decodedData[dataKey] : decodedData;
+      } else {
+        print('Failed to load data from $url: ${response.statusCode}');
       }
-  }
-  Future<void>getPopular()async
-  {
-    var url=Uri.parse(ApiUrls.Popular_List);
-    var res=await http.get(url,headers: {
-      'Content-Type':'application/json',
-      'zoneId':'[1]',
-      'latitude':'23.735129',
-      'longitude':'90.425614'
-    });
-    if(res.statusCode==200)
-    {
-      popular_list.value=jsonDecode(res.body)['products'];
+    } catch (e) {
+      print('Error fetching data from $url: $e');
     }
   }
-  Future<void>getCampaign()async
-  {
-    var url=Uri.parse(ApiUrls.Campaign_List);
-    var res=await http.get(url,headers: {
-      'Content-Type':'application/json',
-      'zoneId':'[1]',
-      'latitude':'23.735129',
-      'longitude':'90.425614'
-    });
-    if(res.statusCode==200)
-    {
-      campaign_list.value=jsonDecode(res.body);
-    }
-  }
-  Future<void>getRestaurants()async
-  {
-    var url=Uri.parse(ApiUrls.Restaurant_List);
-    var res=await http.get(url,headers: {
-      'Content-Type':'application/json',
-      'zoneId':'[1]',
-      'latitude':'23.735129',
-      'longitude':'90.425614'
-    });
-    if(res.statusCode==200)
-    {
-      restaurant_list.value=jsonDecode(res.body)['restaurants'];
-    }
+  Map<String, String> _MyHeaders() {
+    return {
+      'Content-Type': 'application/json',
+      'zoneId': '[1]',
+      'latitude': '23.735129',
+      'longitude': '90.425614',
+    };
   }
 }
--- lib/presentation_layer/ui/pages/app_pages/home.dart
+++ lib/presentation_layer/ui/pages/app_pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:sixbites/presentation_layer/state_holders/home_controller.dart';
 import 'package:sixbites/presentation_layer/ui/Diy_components/home_related/campaign_card.dart';
 import 'package:sixbites/presentation_layer/ui/Diy_components/home_related/category_card.dart';
@@ -14,195 +15,211 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     var mdw=MediaQuery.sizeOf(context).width;
     var mdh=MediaQuery.sizeOf(context).height;
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: false,
-            backgroundColor: Colors.white,
-            elevation: 0,
-            actions: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Stack(
-                  clipBehavior: Clip.none,
-                  children: [
-                    FaIcon(FontAwesomeIcons.solidBell, color: Color(0xFF000101), size: mdw * 0.057),
-                    Positioned(
-                      top: 0,
-                      right: 0,
-                      child: Container(
-                        width: mdw * 0.025,
-                        height: mdw * 0.025,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: Colors.white,
-                            width: mdw*0.003,
+    return Obx((){
+      if(!home.isLoaded.value)
+        {
+          return Scaffold(
+            body: Center(
+              child: LoadingAnimationWidget.twistingDots(
+                leftDotColor: const Color(0xFF030a45),
+                rightDotColor: const Color(0xFF029c55),
+                size: mdw*0.12,
+              ),
+            ),
+          );
+        }
+      else{
+        return Scaffold(
+          backgroundColor: Colors.white,
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                pinned: false,
+                backgroundColor: Colors.white,
+                elevation: 0,
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        FaIcon(FontAwesomeIcons.solidBell, color: Color(0xFF000101), size: mdw * 0.057),
+                        Positioned(
+                          top: 0,
+                          right: 0,
+                          child: Container(
+                            width: mdw * 0.025,
+                            height: mdw * 0.025,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Colors.white,
+                                width: mdw*0.003,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
+                  ),
+                ],
+                title: Row(
+                  children: [
+                    Icon(Icons.home_filled,size: mdw*0.057,color: Color(0xFFcdcdcd),),
+                    SizedBox(width: mdw*0.011),
+                    Text("Mirpur,Dhaka,Bangladesh",style: TextStyle(fontSize: mdw*0.043,color: Color(0xFFcdcdcd)),)
                   ],
                 ),
               ),
-            ],
-            title: Row(
-              children: [
-                Icon(Icons.home_filled,size: mdw*0.057,color: Color(0xFFcdcdcd),),
-                SizedBox(width: mdw*0.011),
-                Text("Mirpur,Dhaka,Bangladesh",style: TextStyle(fontSize: mdw*0.043,color: Color(0xFFcdcdcd)),)
-              ],
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: CustomTextfield(mdw, mdh),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              margin: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Container(
-                    child: Row(
-                      children: [
-                        Text("Categories",style: TextStyle(fontSize: mdw*0.057,fontWeight: FontWeight.bold,color: Color(0xFF010742)),),
-                        Text("View All",style: TextStyle(fontSize: mdw*0.044,color: Color(0xFF18a563),decoration: TextDecoration.underline,decorationColor: Color(0xFF18a563),fontWeight: FontWeight.bold),)
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    ),
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Container(
-                      child:Obx((){
-                        return Row(
-                          children:List.generate(home.category_list.length, (index) {
-                            return CategoryCard(mdw, mdh, "${home.category_list[index]['image_full_url']}", "${home.category_list[index]['name']}");
-                          },),
-                        );
-                      }),
-                    ),
-                  )
-                ],
+              SliverToBoxAdapter(
+                child: CustomTextfield(mdw, mdh),
               ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              margin: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Container(
-                    child: Row(
-                      children: [
-                        Text("Popular Food Nearby",style: TextStyle(fontSize: mdw*0.057,fontWeight: FontWeight.bold,color: Color(0xFF010742)),),
-                        Text("View All",style: TextStyle(fontSize: mdw*0.044,color: Color(0xFF18a563),decoration: TextDecoration.underline,decorationColor: Color(0xFF18a563),fontWeight: FontWeight.bold),)
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    ),
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            Text("Categories",style: TextStyle(fontSize: mdw*0.057,fontWeight: FontWeight.bold,color: Color(0xFF010742)),),
+                            Text("View All",style: TextStyle(fontSize: mdw*0.044,color: Color(0xFF18a563),decoration: TextDecoration.underline,decorationColor: Color(0xFF18a563),fontWeight: FontWeight.bold),)
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        ),
+                      ),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Container(
+                          child:Obx((){
+                            return Row(
+                              children:List.generate(home.category_list.length, (index) {
+                                return CategoryCard(mdw, mdh, "${home.category_list[index]['image_full_url']}", "${home.category_list[index]['name']}");
+                              },),
+                            );
+                          }),
+                        ),
+                      )
+                    ],
                   ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Container(
-                      child:Obx((){
-                        return Row(
-                          children:List.generate(home.popular_list.length, (index) {
-                            return PopularCard(mdw, mdh, "${home.popular_list[index]['image_full_url']}", "${home.popular_list[index]['name']}", "${home.popular_list[index]['restaurant_name']}", home.popular_list[index]['price'], double.parse(home.popular_list[index]['avg_rating'].toStringAsFixed(1)));
-                          },),
-                        );
-                      }),
-                    ),
-                  )
-                ],
-            ),
-          )
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              margin: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Container(
-                    child: Row(
-                      children: [
-                        Text("Food Campaign",style: TextStyle(fontSize: mdw*0.057,fontWeight: FontWeight.bold,color: Color(0xFF010742)),),
-                        Text("View All",style: TextStyle(fontSize: mdw*0.044,color: Color(0xFF18a563),decoration: TextDecoration.underline,decorationColor: Color(0xFF18a563),fontWeight: FontWeight.bold),)
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    ),
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Container(
-                      child:Obx((){
-                        return Row(
-                          children:List.generate(home.campaign_list.length, (index) {
-                            // return CampaignCard(mdw, mdh, "${home.popular_list[index]['image_full_url']}", "${home.popular_list[index]['name']}", "${home.popular_list[index]['restaurant_name']}", home.popular_list[index]['price'], double.parse(home.popular_list[index]['avg_rating'].toStringAsFixed(1)));
-                            return CampaignCard(mdw, mdh, "${home.campaign_list[index]['image_full_url']}", "${home.campaign_list[index]['name']}", "${home.campaign_list[index]['restaurant_name']}", "${home.popular_list[index]['price']}",  "${home.popular_list[index]['discount']}","${home.popular_list[index]['rating_count']}" );
-                          },),
-                        );
-                      }),
-                    ),
-                  )
-                ],
+                ),
               ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              margin: EdgeInsets.only(
-                left: 10,
-                right: 10,
-                top: 10
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              SliverToBoxAdapter(
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    child: Column(
                       children: [
-                        Text(
-                          "All Restaurants",
-                          style: TextStyle(
-                            fontSize: mdw * 0.057,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF010742),
+                        Container(
+                          child: Row(
+                            children: [
+                              Text("Popular Food Nearby",style: TextStyle(fontSize: mdw*0.057,fontWeight: FontWeight.bold,color: Color(0xFF010742)),),
+                              Text("View All",style: TextStyle(fontSize: mdw*0.044,color: Color(0xFF18a563),decoration: TextDecoration.underline,decorationColor: Color(0xFF18a563),fontWeight: FontWeight.bold),)
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           ),
                         ),
-                        Icon(
-                          Icons.filter_list,
-                          size: mdw * 0.057,
-                          color: Color(0xFF010742),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Container(
+                            child:Obx((){
+                              return Row(
+                                children:List.generate(home.popular_list.length, (index) {
+                                  return PopularCard(mdw, mdh, "${home.popular_list[index]['image_full_url']}", "${home.popular_list[index]['name']}", "${home.popular_list[index]['restaurant_name']}", home.popular_list[index]['price'], double.parse(home.popular_list[index]['avg_rating'].toStringAsFixed(1)));
+                                },),
+                              );
+                            }),
+                          ),
                         )
                       ],
                     ),
-                  ),
-                  Container(
-                    child: Obx((){
-                      return ListView.separated(
-                        shrinkWrap: true,
-                        physics: NeverScrollableScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          return RestaurantCard(mdw, mdh, "${home.restaurant_list[index]['logo_full_url']}", "${home.restaurant_list[index]['name']}", "${home.restaurant_list[index]['address']}", "${home.restaurant_list[index]['reviews_comments_count']}","${home.restaurant_list[index]['avg_rating']}");
-                        },
-                        separatorBuilder: (context, index) => Divider(
-                          indent: mdw*0.3,
-                          endIndent: mdw*0.03,
-                        ),
-                        itemCount: home.restaurant_list.length,
-                      );
-                    }),
                   )
-                ],
               ),
-            ),
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            Text("Food Campaign",style: TextStyle(fontSize: mdw*0.057,fontWeight: FontWeight.bold,color: Color(0xFF010742)),),
+                            Text("View All",style: TextStyle(fontSize: mdw*0.044,color: Color(0xFF18a563),decoration: TextDecoration.underline,decorationColor: Color(0xFF18a563),fontWeight: FontWeight.bold),)
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        ),
+                      ),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Container(
+                          child:Obx((){
+                            return Row(
+                              children:List.generate(home.campaign_list.length, (index) {
+                                // return CampaignCard(mdw, mdh, "${home.popular_list[index]['image_full_url']}", "${home.popular_list[index]['name']}", "${home.popular_list[index]['restaurant_name']}", home.popular_list[index]['price'], double.parse(home.popular_list[index]['avg_rating'].toStringAsFixed(1)));
+                                return CampaignCard(mdw, mdh, "${home.campaign_list[index]['image_full_url']}", "${home.campaign_list[index]['name']}", "${home.campaign_list[index]['restaurant_name']}", "${home.popular_list[index]['price']}",  "${home.popular_list[index]['discount']}","${home.popular_list[index]['rating_count']}" );
+                              },),
+                            );
+                          }),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  margin: EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      top: 10
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "All Restaurants",
+                              style: TextStyle(
+                                fontSize: mdw * 0.057,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF010742),
+                              ),
+                            ),
+                            Icon(
+                              Icons.filter_list,
+                              size: mdw * 0.057,
+                              color: Color(0xFF010742),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Obx((){
+                          return ListView.separated(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              return RestaurantCard(mdw, mdh, "${home.restaurant_list[index]['logo_full_url']}", "${home.restaurant_list[index]['name']}", "${home.restaurant_list[index]['address']}", "${home.restaurant_list[index]['reviews_comments_count']}","${home.restaurant_list[index]['avg_rating']}");
+                            },
+                            separatorBuilder: (context, index) => Divider(
+                              indent: mdw*0.3,
+                              endIndent: mdw*0.03,
+                            ),
+                            itemCount: home.restaurant_list.length,
+                          );
+                        }),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-      bottomNavigationBar: CustomNavbar(mdw: mdw, mdh: mdh),
-    );
+          bottomNavigationBar: CustomNavbar(mdw: mdw, mdh: mdh),
+        );
+      }
+    });
   }
 }
