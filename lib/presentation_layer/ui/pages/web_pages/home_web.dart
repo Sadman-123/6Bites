4deb55ee828b65d73138846f394a1d5d869b4b18
Sadman-123 <19201103123@cse.bubt.edu.bd>
2025-01-30 21:38:25 +0600

    web Banner part done

--- lib/presentation_layer/ui/pages/web_pages/home_web.dart
+++ lib/presentation_layer/ui/pages/web_pages/home_web.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -5,7 +6,6 @@ import 'package:get/get.dart';
 import 'package:sixbites/presentation_layer/state_holders/home_controller.dart';
 import 'package:sixbites/presentation_layer/ui/Diy_components/web_related/web_campaign_card.dart';
 import 'package:sixbites/presentation_layer/ui/Diy_components/web_related/web_category_card.dart';
-import '../../Diy_components/home_related/campaign_card.dart';
 import '../../Diy_components/web_related/web_popular_card.dart';
 class HomeWeb extends StatelessWidget {
   HomeController home=Get.find();
@@ -146,6 +146,59 @@ class HomeWeb extends StatelessWidget {
                                 ),
                               ),
                             ) ,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            child:  Obx(() {
+                              return Container(
+                                child: Column(
+                                  children: [
+                                    CarouselSlider(
+                                      items: List.generate(home.banner_list.length, (index) {
+                                        return Container(
+                                          width: mdw*0.46,
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(10),
+                                            image: DecorationImage(
+                                              image: NetworkImage("${home.banner_list[index]['image_full_url']}"),
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                        );
+                                      }),
+                                      options: CarouselOptions(
+                                        height: mdh * 0.385,
+                                        autoPlay: true,
+                                        enlargeCenterPage: true,
+                                        aspectRatio: 16 / 8,
+                                        autoPlayCurve: Curves.fastOutSlowIn,
+                                        enableInfiniteScroll: true,
+                                        autoPlayAnimationDuration: Duration(milliseconds: 800),
+                                        viewportFraction: 0.5,
+                                        onPageChanged: (index, reason) {
+                                          home.currentIndex.value = index;
+                                        },
+                                      ),
+                                    ),
+                                    SizedBox(height: mdh*0.029),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: List.generate(home.banner_list.length, (index) {
+                                        return Container(
+                                          width: mdw*0.015,
+                                          height: mdh*0.015,
+                                          margin: EdgeInsets.symmetric(horizontal: 4.0),
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: index==home.currentIndex.value ? Color(0xFF029c55) : Colors.grey,
+                                          ),
+                                        );
+                                      }),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }),
                           ),
                           Container(
                             margin: EdgeInsets.all(10),
