78258f8fe3a58f3a2e1b9f0d17305cb2d76e0fec
Sadman-123 <19201103123@cse.bubt.edu.bd>
2025-02-01 11:29:03 +0600

    changes on app_main_screen.dart

--- lib/presentation_layer/ui/screens/app_main_screen.dart
+++ lib/presentation_layer/ui/screens/app_main_screen.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 import 'package:sixbites/presentation_layer/state_holders/home_controller.dart';
+import 'package:sixbites/presentation_layer/styles/app_colors.dart';
 import 'package:sixbites/presentation_layer/ui/screens/app_pages/cart_app.dart';
 import 'package:sixbites/presentation_layer/ui/screens/app_pages/favourite_app.dart';
 import 'package:sixbites/presentation_layer/ui/screens/app_pages/home_app.dart';
@@ -21,8 +22,8 @@ class AppMainScreen extends StatelessWidget{
           appBar: AppBar(backgroundColor: Colors.white,),
           body: Center(
             child: LoadingAnimationWidget.twistingDots(
-              leftDotColor: const Color(0xFF030a45),
-              rightDotColor: const Color(0xFF029c55),
+              leftDotColor: AppColors.secondaryColor,
+              rightDotColor: AppColors.primaryColor,
               size: mdw*0.12,
             ),
           ),
@@ -40,53 +41,25 @@ class AppMainScreen extends StatelessWidget{
            MenuApp(),
          ],
          items: [
-           PersistentBottomNavBarItem(
-             icon: Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: FaIcon(FontAwesomeIcons.house),
-             ),
-             activeColorPrimary: Color(0xFF029c55),
-             inactiveColorPrimary: Colors.grey,
-             iconSize: mdw*0.055,
-           ),
-           PersistentBottomNavBarItem(
-             icon: Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: FaIcon(FontAwesomeIcons.solidHeart),
-             ),
-             activeColorPrimary: Color(0xFF029c55),
-             inactiveColorPrimary: Colors.grey,
-             iconSize: mdw*0.055,
-           ),
-           PersistentBottomNavBarItem(
-             icon: Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Icon(Icons.shopping_cart,color: Color(0xFFffffff),),
-             ),
-             activeColorPrimary: Color(0xFF029c55),
-             inactiveColorPrimary: Colors.grey,
-           ),
-           PersistentBottomNavBarItem(
-             icon: Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: FaIcon(FontAwesomeIcons.filePen),
-             ),
-             activeColorPrimary: Color(0xFF029c55),
-             inactiveColorPrimary: Colors.grey,
-             iconSize: mdw*0.055,
-           ),
-           PersistentBottomNavBarItem(
-             icon: Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: FaIcon(FontAwesomeIcons.bars),
-             ),
-             activeColorPrimary: Color(0xFF029c55),
-             inactiveColorPrimary: Colors.grey,
-             iconSize: mdw*0.055,
-           ),
+           _Navbaritem(mdw, FaIcon(FontAwesomeIcons.home)),
+           _Navbaritem(mdw, FaIcon(FontAwesomeIcons.solidHeart)),
+           _Navbaritem(mdw, FaIcon(FontAwesomeIcons.shoppingCart,color: Colors.white,)),
+           _Navbaritem(mdw, FaIcon(FontAwesomeIcons.receipt)),
+           _Navbaritem(mdw, FaIcon(FontAwesomeIcons.bars)),
          ],
        );
      }
    });
+  }
+  PersistentBottomNavBarItem _Navbaritem(double mdw, FaIcon icon) {
+    return PersistentBottomNavBarItem(
+      icon: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: icon,
+      ),
+      activeColorPrimary: AppColors.primaryColor,
+      inactiveColorPrimary: Colors.grey,
+      iconSize: mdw*0.055,
+    );
   }
 }
